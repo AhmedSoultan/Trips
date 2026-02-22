cad853569995d732fc6a8d8f4d471631678684a9
Ahmed <ahmed.sultan@edu.uekat.pl>
2026-02-22 22:13:36 +0100

    Initial Commit

---
+++ Trips/TripsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripsApp.swift
+//  Trips
+//
+//  Created by Ahmed Sultan on 22/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct TripsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TripsUITests/TripsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TripsUITestsLaunchTests.swift
+//  TripsUITests
+//
+//  Created by Ahmed Sultan on 22/02/2026.
+//
+
+import XCTest
+
+final class TripsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
